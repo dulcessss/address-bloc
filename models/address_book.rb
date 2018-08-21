566bec9a04b2d9497b88a1e4476b1c55437b9a5a
dulcessss <dulce.aguilera25@gmail.com>
2018-08-20 21:40:33 -0500

    Checkpoint 6: COMPLETE

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,6 +3,7 @@
 =end
 
 require_relative 'entry'
+require 'csv'
 
   class AddressBook
     attr_reader :entries
@@ -30,5 +31,29 @@ Otherwise we increment 'index' and continue comparing with the other entries.
     # #11 we insert a new entry into 'entries' using the calculated index.
 
     entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+=begin
+#7  we defined import_from_csv. The method starts by reading the file, using
+File.read. The file will be in a CSV format. We use the CSV class to parse the
+file. The result of CSV.parse is an object of type CSV::Table.
+=end
+
+    def import_from_csv(file_name)
+      # Implementations goes here
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+=begin
+  #8 we iterate over the CSV::Table object's rows. On the next line we
+  create a hash for each row. We convert each row_hash to an Entry by using
+  the add_entry method which will also add the Entry to the AddressBook's entries.
+=end
+
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
+
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,33 +1,57 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
+
+=begin
+#1 we create new instance of the AddressBook model and assign it to the variable named book using
+the let syntax provided by RSpec. This lets us use book in all our tests, removing
+the duplication of having to instantiate a new AddressBook for each test.
+=end
+
+  let(:book) { AddressBook.new }
+
+=begin
+#2 we see describe and it statements which are an RSpec paradigm to explain what
+we are testing. it explains the functionality of the method we're testing in a
+human readable form. RSpec will take the content from describe and it and output
+them nicely to the command line when we execute the test.
+=end
+
+
+=begin
+#6 we create a helper method named check_entry which consolidates the redundant
+code. We can now pass in the particular name, number, and email address we
+want into this reusable helper method.
+=end
+
+  def check_entry(entry, expected_name, expected_number, expected_email)
+    expect(entry.name).to eq expected_name
+    expect(entry.phone_number).to eq expected_number
+    expect(entry.email).to eq expected_email
+  end
+
   describe "attributes" do
     it "responds to entries" do
-      book = AddressBook.new
       expect(book).to respond_to(:entries)
     end
 
     it "initializes entries as an array" do
-      book = AddressBook.new
       expect(book.entries).to be_an(Array)
     end
 
     it "initializes entries as empty" do
-      book = AddressBook.new
       expect(book.entries.size).to eq(0)
     end
   end
 
     describe "#add_entry" do
       it "adds only one entry to the address book" do
-        book = AddressBook.new
         book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
         expect(book.entries.size).to eq(1)
       end
 
       it "adds the correct information to entries" do
-        book = AddressBook.new
         book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
         new_entry = book.entries[0]
 
@@ -36,4 +60,74 @@ RSpec.describe AddressBook do
         expect(new_entry.email).to eq('augusta.king@lovelace.com')
       end
     end
+
+# Test that AddressBook's .import_from_csv() method is working as expected
+
+  describe "#import_from_csv" do
+    it "imports the correct number of entries" do
+
+=begin
+#3 after the describe and it statements, we call the import_from_csv method on
+the book object which is of type AddressBook (our data model). We pass
+import_from_csv the string entries.csv as a parameter. CSV files are a fairly
+typical way of dealing with data and you can read more about them here.
+On the next line we reference the AddressBook.entries variable to get its size.
+This variable will be an array. Next, we save the size of the
+AddressBook.entries to our local variable book_size.
+=end
+
+      book.import_from_csv("entries.csv")
+      book_size = book.entries.size
+
+# Check the size of the entries in AddressBook
+
+      expect(book_size).to eq 5
+    end
   end
+    # #4 we access the first entry in the array of entries that our AddressBook stores
+
+    it "imports the 1st entry" do
+     book.import_from_csv("entries.csv")
+     # Check the first entry
+     entry_one = book.entries[0]
+
+=begin
+     # #5 we've added three epects to verify that the first entry has the name "Bill",
+     the phone number "555-555-4854", and the email address "bill@blocmail.com".
+
+=end
+
+    check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+
+   end
+
+  it "imports the 2nd entry" do
+       book.import_from_csv("entries.csv")
+       # Check the second entry
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+
+     end
+
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries.csv")
+       # Check the third entry
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
+
+     it "imports the 4th entry" do
+       book.import_from_csv("entries.csv")
+       # Check the fourth entry
+       entry_four = book.entries[3]
+       check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+
+     end
+
+     it "imports the 5th entry" do
+       book.import_from_csv("entries.csv")
+       # Check the fifth entry
+       entry_five = book.entries[4]
+       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+     end
+   end
