3a9e3cfd1bd99c51d7253883514c17f70fa59be0
dulcessss <dulce.aguilera25@gmail.com>
2018-08-23 17:58:00 -0500

    Checkpoint 7: COMPLETE

--- models/address_book.rb
+++ models/address_book.rb
@@ -44,16 +44,30 @@ file. The result of CSV.parse is an object of type CSV::Table.
       csv_text = File.read(file_name)
       csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
 
-=begin
-  #8 we iterate over the CSV::Table object's rows. On the next line we
-  create a hash for each row. We convert each row_hash to an Entry by using
-  the add_entry method which will also add the Entry to the AddressBook's entries.
-=end
-
       csv.each do |row|
         row_hash = row.to_hash
         add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
       end
+    end
 
-  end
+    def binary_search(name)
+      lower = 0
+      upper = entries.length - 1
+
+      while lower <= upper
+        mid = (lower + upper) / 2
+        mid_name = entries[mid].name
+
+        if name == mid_name
+          return entries[mid]
+        elsif name < mid_name
+          upper = mid - 1
+        elsif name > mid_name
+          lower = mid + 1
+        end
+      end
+
+      return nil
+      
+    end
 end
